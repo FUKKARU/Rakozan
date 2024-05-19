1519053a3539c8759ddb3dd9a94f1504a9c2b3eb
foriver4725 <tk230001@tks.iput.ac.jp>
2024-05-19 18:22:01 +0900

    [Test]

---
+++ Assets/Scripts/Enemy/Getter/HaizuriGetter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Enemy.Getter
+{
+    public class HaizuriGetter : MonoBehaviour
+    {
+        [SerializeField] GameObject self;
+        public GameObject Self => self;
+
+        [SerializeField] Transform tf;
+        public Transform Tf => tf;
+
+        [SerializeField] Collider coll;
+        public Collider Coll => coll;
+
+        [SerializeField] Rigidbody rb;
+        public Rigidbody Rb => rb;
+
+        [SerializeField] Animator anim;
+        public Animator Anim => anim;
+
+        [SerializeField] GameObject model;
+        public GameObject Model => model;
+
+        [SerializeField] GameObject smokes;
+        public GameObject Smokes => smokes;
+    }
+}
---
+++ Assets/Scripts/Test/HaizuriMove.cs
@@ -0,0 +1,25 @@
+using Enemy.Getter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class HaizuriMove : MonoBehaviour
+    {
+        [SerializeField] float speed;
+        HaizuriGetter haizuri;
+
+        void Start()
+        {
+            haizuri = GameObject.FindGameObjectWithTag("Enemy_Haizuri").GetComponent<HaizuriGetter>();
+
+            haizuri.Rb.useGravity = false;
+        }
+
+        void Update()
+        {
+            haizuri.Tf.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
